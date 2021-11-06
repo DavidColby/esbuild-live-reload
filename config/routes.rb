7d083eb9ac2d9ce542dcded754581fb7deeabb38
David Colby <davidlcolby@gmail.com>
2021-11-06 11:11:47 -0500

    Initial commit, project complete

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Almost every application defines a route for the root path ("/") at the top of this file.
+  # root "articles#index"
+end
